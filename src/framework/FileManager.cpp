b0907eb556c792695093025ab088af7f9e443ab7
Tommi Korpelainen <tommi.korpelainen@aalto.fi>
2021-11-19 10:05:40 +0200

    log what is being loaded during Resource loading

--- src/framework/FileManager.cpp
+++ src/framework/FileManager.cpp
@@ -1,5 +1,17 @@
 #include <framework/FileManager.hpp>
+#include <iostream>
 
-bool FileManager::LoadTexture(sf::Texture& texture, const std::string& path) const { return texture.loadFromFile(path); }
-bool FileManager::LoadAudio(sf::SoundBuffer& soundBuffer, const std::string& path) const { return soundBuffer.loadFromFile(path); }
-bool FileManager::LoadFont(sf::Font& font, const std::string& path) const { return font.loadFromFile(path); }
+bool FileManager::LoadTexture(sf::Texture& texture, const std::string& path) const {
+    std::cout << "  Loading texture: " << path << std::endl;
+    return texture.loadFromFile(path);
+}
+
+bool FileManager::LoadAudio(sf::SoundBuffer& soundBuffer, const std::string& path) const {
+    std::cout << "  Loading audio: " << path << std::endl;
+    return soundBuffer.loadFromFile(path);
+}
+
+bool FileManager::LoadFont(sf::Font& font, const std::string& path) const {
+    std::cout << "  Loading font: " << path << std::endl;    
+    return font.loadFromFile(path);
+}
