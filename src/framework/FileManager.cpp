8964439ff2e99e696914d814e7136ff0b7cf6b55
Joonas Palmulaakso <joonas.palmulaakso@aalto.fi>
2021-11-29 04:48:50 +0200

    Untested level saver and loader

--- src/framework/FileManager.cpp
+++ src/framework/FileManager.cpp
@@ -15,3 +15,86 @@ bool FileManager::LoadFont(sf::Font& font, const std::string& path) const {
     std::cout << "  Loading font: " << path << std::endl;    
     return font.loadFromFile(path);
 }
+
+
+void PrintGameObjectData(std::ofstream& file, gm::GameObjectData& data){
+    file << "DATA " << data.x << " " << data.y << " " << data.rot << " " << data.type << "\n";
+}
+
+void PrintHighScores(std::ofstream& file, std::pair<std::string, int>& score){
+    file << "HIGH " << score.first << " " << score.second << "\n";
+}
+
+bool FileManager::SaveLevel(const Level& level, const std::string& path) const{
+    std::ofstream file;
+    file.open(path, std::ios::in);
+    if (!file.is_open()){
+        return false;
+    }
+
+    file << "NAME " << level.levelName << "\n";
+    file << "MODE " << level.levelMode << "\n";
+    
+    for(auto i : level.objectData){
+        PrintGameObjectData(file, i);
+    }
+
+    for(auto i : level.highscores){
+        PrintHighScores(file, i);
+    }
+
+    file << "BACKGROUND " << level.backgroundImage << "\n";
+    file << "END\n";
+
+    file.close();
+    return true;
+}
+
+bool FileManager::LoadLevel(Level& level, const std::string& path) const {
+    std::ifstream file;
+    file.open(path, std::ios::out);
+    if (!file.is_open()){
+        return false;
+    }
+    std::string line;
+    while(std::getline(file, line)){
+        std::istringstream ss(line);
+        std::vector<std::string> lineData;
+        std::string temp;
+
+        while (std::getline(ss, temp, ' ')){
+            lineData.push_back(temp);
+        }
+
+        if (lineData[0] == "NAME"){
+            level.levelName = lineData[1];
+        }   else if (lineData[0] == "MODE"){
+            int num = std::stoi(lineData[1]);
+            LevelMode mode = static_cast<LevelMode>(num); //+1?
+            level.levelMode = mode;
+
+        }   else if (lineData[0] == "DATA"){
+            gm::GameObjectData object;
+            object.x = std::stof(lineData[1]);
+            object.y = std::stof(lineData[2]);
+            object.rot = std::stof(lineData[3]);
+            object.type = static_cast<gm::GameObjectType>(std::stoi(lineData[4]));
+            level.objectData.push_back(object);
+
+        }   else if (lineData[0] == "HIGH"){
+            std::pair<std::string, int> pair;
+            pair.first = lineData[1];
+            pair.second = std::stoi(lineData[2]);
+            level.highscores.push_back(pair);
+
+        }   else if (lineData[0] == "BACKGROUND"){
+            level.backgroundImage = static_cast<SpriteID>(stoi(lineData[1]));
+            
+        }   else if (lineData[0] == "END"){
+            file.close();
+            return true;
+        }
+    }
+    file.close();
+    return false; //Didn't end in END line, so propably some problems in file
+}
